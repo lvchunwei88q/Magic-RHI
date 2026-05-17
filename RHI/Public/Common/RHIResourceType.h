fb5f680b213eb170267b573acd30cc28ab52de6a
lvchunwei88q <213587437@qq.com>
2026-05-17 11:57:01 +0800

    加入RHI的资源基类，加入资源类型枚举，加入Readme

---
+++ RHI/Public/Common/RHIResourceType.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <cstdint>
+
+/** An enumeration of the different RHI reference types. */
+enum RHIResourceType : uint8_t
+{
+	RRT_None,
+
+	RRT_SamplerState,
+	RRT_RasterizerState,
+	RRT_DepthStencilState,
+	RRT_BlendState,
+	RRT_VertexDeclaration,
+	RRT_VertexShader,
+	RRT_MeshShader,
+	RRT_AmplificationShader,
+	RRT_PixelShader,
+	RRT_GeometryShader,
+	RRT_RayTracingShader,
+	RRT_ComputeShader,
+	RRT_GraphicsPipelineState,
+	RRT_ComputePipelineState,
+	RRT_RayTracingPipelineState,
+	RRT_BoundShaderState,
+	RRT_UniformBufferLayout,
+	RRT_UniformBuffer,
+	RRT_Buffer,
+	RRT_Texture,
+	RRT_TextureReference,
+	RRT_TimestampCalibrationQuery,
+	RRT_GPUFence,
+	RRT_RenderQuery,
+	RRT_RenderQueryPool,
+	RRT_Viewport,
+	RRT_UnorderedAccessView,
+	RRT_ShaderResourceView,
+	RRT_RayTracingAccelerationStructure,
+	RRT_RayTracingShaderBindingTable,
+	RRT_StagingBuffer,
+	RRT_CustomPresent,
+	RRT_ShaderLibrary,
+	RRT_ShaderBundle,
+	RRT_WorkGraphShader,
+	RRT_WorkGraphPipelineState,
+	RRT_StreamSourceSlot,
+	RRT_ResourceCollection,
+
+	RRT_Num
+};
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# DirectXRHI 
+
+本项目使用MSVC工具链开发，并且使用Trae AI辅助，目前处于快速开发期，并且内置了Build.bat脚本，使用他就可以构建项目
