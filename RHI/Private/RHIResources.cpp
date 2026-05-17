3fcbfb9a55ac5ca0de98867d844c9b642dc5ed3d
lvchunwei88q <213587437@qq.com>
2026-05-17 11:57:01 +0800

    加入RHI的资源基类，加入资源类型枚举，加入Readme

---
+++ RHI/Private/RHIResources.cpp
@@ -0,0 +1,10 @@
+#include <RHIResource.h>
+
+namespace RHI
+{       
+#if RHI_ENABLE_RESOURCE_INFO
+        void RHIResource::GetInfo(RHIResourceInfo& OutInfo) const{
+            OutInfo = RHIResourceInfo{ /* NOT */ };
+        }
+#endif
+}
---
+++ RHI/Public/RHIResource.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <Common/RHI_API.h>
+#include <Common/RHIResourceType.h>
+#include <string>
+#include <atomic>
+
+#ifndef RHI_ENABLE_RESOURCE_INFO
+    #ifdef _DEBUG
+        #define RHI_ENABLE_RESOURCE_INFO 1   // Debug: Run
+    #else
+        #define RHI_ENABLE_RESOURCE_INFO 0   // Release: Close
+    #endif
+#endif
+
+namespace RHI
+{  
+    struct GPUVRamAllocation
+    {
+        GPUVRamAllocation() = default;
+        GPUVRamAllocation(uint64_t InAllocationStart, uint64_t InAllocationSize)
+            : AllocationStart(InAllocationStart)
+            , AllocationSize(InAllocationSize)
+        {
+        }
+
+        bool IsValid() const { return AllocationSize > 0; }
+        
+        // in bytes
+        uint64_t AllocationStart{};
+        // in bytes
+        uint64_t AllocationSize{};
+    };
+
+    struct RHIResourceInfo
+    {
+        std::string Name;
+        RHIResourceType Type{ RRT_None };
+        GPUVRamAllocation VRamAllocation;
+        bool IsTransient{ false };
+        bool bValid{ true };
+        bool bResident{ true };
+    };
+
+    /** The base type of RHI resources. */
+    class RHI_API RHIResource
+    {
+    public:
+        RHIResource(RHIResourceType InType) : Type(InType) {}
+        virtual ~RHIResource() = default;
+        
+        // Copying is prohibited
+        RHIResource(const RHIResource&) = delete;
+        RHIResource& operator=(const RHIResource&) = delete;
+        
+        // Reference counting
+        uint32_t AddRef()
+        {
+            return ++RefCount;
+        }
+        
+        uint32_t Release()
+        {
+            uint32_t NewCount = --RefCount;
+            if (NewCount == 0)
+            {
+                delete this;
+            }
+            return NewCount;
+        }
+        
+        uint32_t GetRefCount() const { return RefCount; }
+        
+        // type
+        RHIResourceType GetType() const { return Type; }
+        
+#if RHI_ENABLE_RESOURCE_INFO
+        virtual void GetInfo(RHIResourceInfo& OutInfo) const;
+#endif
+
+    protected:
+        std::atomic<uint32_t> RefCount{ 1 }; 
+        RHIResourceType Type;
+    };
+} // namespace RHI
